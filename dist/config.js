d4b7a5e919fcbe147e3715efef3a08c3a5419cab
Hai Phan <hai.phan@gmail.com>
2018-11-29 18:46:06 -0800

    use fetch/reset instead of pull

--- dist/config.js
+++ dist/config.js
@@ -15,7 +15,7 @@ exports.default = {
             startService: "cd ${deployFolder}/${serviceName} && npm start 1>stdout.log 2>stderr.log",
             killService: "kill -9 ${pid}",
             checkService: "timeout ${timeout} tail -f --pid=${pid} /dev/null; kill -0 ${pid}",
-            updateService: "cd ${deployFolder}/${serviceName} && git pull && npm i --only=prod --no-save",
+            updateService: "cd ${deployFolder}/${serviceName} && git fetch origin master && git reset --hard origin/master && npm i --only=prod --no-save",
             viewServiceLogs: "cd ${deployFolder}/${serviceName} && touch stdout.log stderr.log && tail -n ${lines} stdout.log && tail -n ${lines} stderr.log 1>&2",
         },
         windows: {
@@ -26,7 +26,7 @@ exports.default = {
             startService: "cd ${deployFolder}\\${serviceName} && npm start 1>stdout.log 2>stderr.log",
             killService: "taskkill /F /PID ${pid}",
             checkService: "powershell Wait-Process -Id ${pid} -Timeout ${timeout}; Get-Process -Id ${pid}",
-            updateService: "cd ${deployFolder}\\${serviceName} && git pull && npm i --only=prod --no-save",
+            updateService: "cd ${deployFolder}\\${serviceName} && git fetch origin master && git reset --hard origin/master && npm i --only=prod --no-save",
             viewServiceLogs: "cd ${deployFolder}\\${serviceName} && (if not exist stdout.log copy NUL stdout.log) && (if not exist stderr.log copy NUL stderr.log) && powershell Get-Content -Tail ${lines} stdout.log && powershell Get-Content -Tail ${lines} stderr.log 1>&2",
         }
     },
