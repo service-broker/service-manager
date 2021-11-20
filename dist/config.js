8ea401046f02b6df110d6452973b0b9e1eb5ac3b
Hai Phan <hai.phan@gmail.com>
2021-11-19 19:33:28 -0800

    process.env.START_COMMAND instead of 'npm start'

--- dist/config.js
+++ dist/config.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a, _b;
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv = require("dotenv");
 const assert = require("assert");
@@ -15,7 +16,7 @@ exports.default = {
             readServiceConf: "cat ${deployFolder}/${serviceName}/.env",
             deployService: "cd ${deployFolder} && git clone ${repoUrl} ${serviceName} && cd ${serviceName} && npm i --only=prod --no-save",
             undeployService: "rm -rf ${deployFolder}/${serviceName}",
-            startService: "cd ${deployFolder}/${serviceName} && npm start 1>stdout.log 2>stderr.log",
+            startService: "cd ${deployFolder}/${serviceName} && " + ((_a = process.env.START_COMMAND) !== null && _a !== void 0 ? _a : "npm start") + " 1>stdout.log 2>stderr.log",
             killService: "kill -9 ${pid}",
             checkService: "timeout ${timeout} tail -f --pid=${pid} /dev/null; kill -0 ${pid}",
             updateService: "cd ${deployFolder}/${serviceName} && git fetch origin master && git reset --hard origin/master && npm i --only=prod --no-save",
@@ -26,7 +27,7 @@ exports.default = {
             readServiceConf: "type ${deployFolder}\\${serviceName}\\.env",
             deployService: "cd ${deployFolder} && git clone ${repoUrl} ${serviceName} && cd ${serviceName} && npm i --only=prod --no-save",
             undeployService: "rmdir /S /Q ${deployFolder}\\${serviceName}",
-            startService: "cd ${deployFolder}\\${serviceName} && npm start 1>stdout.log 2>stderr.log",
+            startService: "cd ${deployFolder}\\${serviceName} && " + ((_b = process.env.START_COMMAND) !== null && _b !== void 0 ? _b : "npm start") + " 1>stdout.log 2>stderr.log",
             killService: "taskkill /F /PID ${pid}",
             checkService: "powershell Wait-Process -Id ${pid} -Timeout ${timeout}; Get-Process -Id ${pid}",
             updateService: "cd ${deployFolder}\\${serviceName} && git fetch origin master && git reset --hard origin/master && npm i --only=prod --no-save",
