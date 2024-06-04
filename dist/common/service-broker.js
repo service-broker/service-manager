690a719c3748238eb5605c41013566681cd7ac95
Hai Phan <hai.phan@gmail.com>
2024-06-04 21:43:33 +0700

    update depends

--- dist/common/service-broker.js
+++ dist/common/service-broker.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const service_broker_client_1 = require("@service-broker/service-broker-client");
 const config_1 = require("../config");
 const logger_1 = require("./logger");
-const defaultServiceBroker = new service_broker_client_1.ServiceBroker(config_1.default.serviceBrokerUrl, logger_1.default);
+const defaultServiceBroker = new service_broker_client_1.ServiceBroker({ url: config_1.default.serviceBrokerUrl, logger: logger_1.default });
 exports.default = defaultServiceBroker;
